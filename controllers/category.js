f9fd5121c10fd3278eb6d870c22dc71bc3b98689
airdy <jalbot17@gmail.com>
2020-01-13 00:32:21 +0200

    controllers updated

--- controllers/category.js
+++ controllers/category.js
@@ -1,15 +1,45 @@
-module.exports.getAll = (req, res) => {
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
+module.exports.getAll = async (req, res) => {
+    try{
+        const categories = await Category.find({user: req.usage.id});
+        res.status(200).json(categories)
+    } catch (e) {
+    errorHandler(res,e)
+    }
 }
-module.exports.getById = (req, res) => {
-
+module.exports.getById = async (req, res) => {
+    try{
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category)
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
-module.exports.delete = (req, res) => {
-
+module.exports.delete = async (req, res) => {
+    try{
+    await Category.remove({_id: req.params.id});
+    await Position.remove({category: req.params.id})
+        res.status(200).json({
+            message: 'Category deleted'
+        })
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
 module.exports.create = (req, res) => {
+    try{
 
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
 module.exports.update = (req, res) => {
+    try{
 
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
